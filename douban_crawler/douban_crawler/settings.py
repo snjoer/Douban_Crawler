32415aa90187baedeae3379ebed503e48736f79b
Rafael-Cheng <rafaelcheng13@gmail.com>
2017-03-26 14:58:22 +0800

    enable pipeline

--- douban_crawler/douban_crawler/settings.py
+++ douban_crawler/douban_crawler/settings.py
@@ -73,9 +73,10 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
+ITEM_PIPELINES = {
 #    'douban_crawler.pipelines.SomePipeline': 300,
-#}
+    'scrapy_redis.pipeline.RedisPipeline': 300
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
