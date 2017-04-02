f110f8ce8c380870f28f702ffb1eb34fa8bd362e
Rafael-Cheng <rafaelcheng13@gmail.com>
2017-04-02 11:56:28 +0800

    movie content spider

--- douban_crawler/douban_crawler/spiders/movieContentSpider.py
+++ douban_crawler/douban_crawler/spiders/movieContentSpider.py
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-
 '''
 
 This spider extracts specific content from given movie links which can be 
@@ -8,14 +9,35 @@ as redis_key "more_reviews".
 
 '''
 
+import os
+import sys
 import scrapy
 from scrapy_redis.spiders import RedisSpider
 from douban_crawler.items import MovieItem
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 class MovieContentSpider(RedisSpider):
     name = "movieContent"
     redis_key = "movie_links"
 
     def parse(self, response):
-        # todo
-        pass
+        item = MovieItem()
+        name = response.xpath('//h1/span/text()').extract()[0]
+        director = response.xpath('//a[@rel="v:directedBy"]/text()').extract()[0]
+        time = response.xpath('//span[@property="v:initialReleaseDate"]/text()').extract()
+        pls = response.xpath('//span[@class="pl"]')
+        for pl in pls:
+            text_list = pl.xpath('./text()').extract()
+            if len(text_list) > 0:
+                if text_list[0] == '制片国家/地区:':
+                    country = pl.xpath('./following::text()').extract()[0]
+        item['MovieName'] = name
+        item['Director'] = director
+        item['ReleaseTime'] = ','.join(time)
+        item['Country'] = country
+        more_reviews = response.url + 'reviews'
+        command = 'redis-cli lpush more_reviews ' + more_reviews
+        os.system(command)
+        yield item
--- douban_crawler/insert.sh
+++ douban_crawler/insert.sh
@@ -3,5 +3,8 @@
 # Er ist wieder da
 # redis-cli lpush more_reviews https://movie.douban.com/subject/26585014/reviews
 
+# La Danseuse
+redis-cli lpush movie_links https://movie.douban.com/subject/26379657
+
 # Logan
-redis-cli lpush more_reviews https://movie.douban.com/subject/25765735/reviews
+#redis-cli lpush movie_links https://movie.douban.com/subject/25765735
