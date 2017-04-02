5b702e96ece75a2d991730bd60feb5e4bcbf9b22
Qingwei Wang <734393950@qq.com>
2017-04-02 23:04:59 +0800

    Update movieContentSpider.py

--- douban_crawler/douban_crawler/spiders/movieContentSpider.py
+++ douban_crawler/douban_crawler/spiders/movieContentSpider.py
@@ -7,15 +7,45 @@ Moreover, this spider should export link to more reviews to Redis Database
 as redis_key "more_reviews".
 
 '''
-
-import scrapy
+import re,os
 from scrapy_redis.spiders import RedisSpider
 from douban_crawler.items import MovieItem
+from bs4 import BeautifulSoup
 
 class MovieContentSpider(RedisSpider):
     name = "movieContent"
     redis_key = "movie_links"
 
     def parse(self, response):
-        #todo
-        pass
+        data = response.body
+        soup = BeautifulSoup(data,'"html.parser"')
+        item = MovieItem()
+        #MovieName
+        for i in soup.find_all('h1'):
+            p_1 = re.compile('\s+')
+            p_2 = re.compile('\'')
+            new_string = re.sub(p_1, '', i.text)
+            new_string = re.sub(p_2, '|', new_string)
+            MovieName = new_string
+        #Director
+        for i in soup.find_all('a', rel='v:directedBy'):
+            Director = i.text
+        #ReleaseTime
+        for i in soup.find_all('span', property='v:initialReleaseDate'):
+            ReleaseTime = i.text
+        #Country
+        info = soup.find('div', {'id': 'info'})
+        span = info.find_all('span', {'class': 'pl'})[4]
+        p_1 = re.compile('\s+')
+        Country = re.sub(p_1, '', span.nextSibling)
+
+        item['MovieName'] = MovieName
+        item['Director'] = Director
+        item['ReleaseTime'] = ReleaseTime
+        item['Country'] = Country
+        yield item
+
+        #comment_url
+        comment_url = soup.find_all('div', class_='mod-hd')[0].find_all('a', class_=None)[0]['href']
+        command = "redis-cli lpush more_reviews " + comment_url
+        os.system(command)
