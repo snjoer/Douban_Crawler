9ff77bba68ea7562bcc7a100db28e667c4775839
Rafael-Cheng <rafaelcheng13@gmail.com>
2017-04-06 17:20:02 +0800

    modify insert.sh

--- douban_crawler/insert.sh
+++ douban_crawler/insert.sh
@@ -3,5 +3,7 @@
 # Er ist wieder da
 # redis-cli lpush more_reviews https://movie.douban.com/subject/26585014/reviews
 
+redis-cli lpush review_links https://movie.douban.com/review/8449807/
+
 # Logan
-redis-cli lpush review_links  https://movie.douban.com/review/8359263/
+# redis-cli lpush review_links  https://movie.douban.com/review/8359263/
