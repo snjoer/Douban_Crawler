0ecfa48fe02a996d400e33df3c60f763c5c3c6a5
redcake18 <1085278033@qq.com>
2017-04-07 07:04:35 -0700

    modify insert.sh

--- douban_crawler/insert.sh
+++ douban_crawler/insert.sh
@@ -1,7 +1,7 @@
 # This is a bash script which helps to insert keys to redis database.
 
 # Er ist wieder da
-# redis-cli lpush more_reviews https://movie.douban.com/subject/26585014/reviews
+redis-cli lpush more_reviews https://movie.douban.com/subject/26585014/reviews
 
 # Logan
-redis-cli lpush more_reviews https://movie.douban.com/subject/25765735/reviews
+#redis-cli lpush more_reviews https://movie.douban.com/subject/25765735/reviews
