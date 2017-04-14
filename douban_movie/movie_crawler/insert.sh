b31f14867839ca4c8cf96c550a5ff3fcb086a77f
Rafael-Cheng <rafaelcheng13@gmail.com>
2017-04-14 15:59:08 +0800

    clone git movie

---
+++ douban_movie/movie_crawler/insert.sh
@@ -0,0 +1,13 @@
+# This is a bash script which helps to insert keys to redis database.
+
+# Er ist wieder da
+redis-cli lpush more_reviews https://movie.douban.com/subject/26585014/reviews
+
+redis-cli lpush review_links https://movie.douban.com/review/8449807/
+
+# Logan
+<<<<<<< HEAD
+#redis-cli lpush more_reviews https://movie.douban.com/subject/25765735/reviews
+=======
+# redis-cli lpush review_links  https://movie.douban.com/review/8359263/
+>>>>>>> Douban_Crawler_RC/master
