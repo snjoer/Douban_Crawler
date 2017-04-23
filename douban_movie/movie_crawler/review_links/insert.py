7db5303601cae8e01a9990835505480fe4b91c7f
Rafael-Cheng <rafaelcheng13@gmail.com>
2017-04-23 21:47:44 +0800

    update insert

--- douban_movie/movie_crawler/review_links/insert.py
+++ douban_movie/movie_crawler/review_links/insert.py
@@ -5,7 +5,14 @@ import os
 import sys
 
 if __name__ == '__main__':
+    start = int(sys.argv[2])
     with open(sys.argv[1], 'r') as f:
         lines = f.readlines()
+        i = 0
+        end = 100 + start
         for line in lines:
-            os.system('redis-cli lpush more_reviews ' + line)   
+            if i >= start:
+                if i == end:
+                    break
+                os.system('redis-cli lpush more_reviews ' + line)  
+            i += 1
