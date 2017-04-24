b10b5c64b3aa8c4b52135f753371dd914d7a5975
Rafael-Cheng <rafaelcheng13@gmail.com>
2017-04-24 13:24:39 +0800

    abort change

--- douban_movie/movie_crawler/review_links/insert.py
+++ douban_movie/movie_crawler/review_links/insert.py
@@ -5,14 +5,6 @@ import os
 import sys
 
 if __name__ == '__main__':
-    start = int(sys.argv[2])
     with open(sys.argv[1], 'r') as f:
-        lines = f.readlines()
-        i = 0
-        end = 100 + start
         for line in lines:
-            if i >= start:
-                if i == end:
-                    break
-                os.system('redis-cli lpush more_reviews ' + line)  
-            i += 1
+            os.system('redis-cli lpush more_reviews ' + line)  
