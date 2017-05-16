fa6b16c33219e773b92e581fff2adec5224fbb7d
Rafael-Cheng <rafaelcheng13@gmail.com>
2017-05-16 08:37:19 +0800

    change before pull

--- douban_user/user_crawler/douban_crawler/spiders/userSpider.py
+++ douban_user/user_crawler/douban_crawler/spiders/userSpider.py
@@ -1,5 +1,4 @@
 '''
-
 This spider gets start url using key "start_url".
 It fetches user data and yield corresponding item.
 Yield requests of his/her following Users
@@ -7,6 +6,7 @@ Yield requests of his/her following Users
 '''
 
 import re
+import json
 import scrapy
 from scrapy_redis.spiders import RedisSpider
 from douban_crawler.items import UserItem
@@ -16,15 +16,46 @@ class DoubanUserSpider(RedisSpider):
     redis_key = "start_url"
 
     def parse(self, response):
-        # call getInfo to get user info.
-        # call getFollowingUsers to get following user link.
-        # yield item and requests.
-        pass
-    
+        item = UserItem()
+        item['UserName'] = ''.join(response.\
+                xpath('//div[@class="name"]/text()').\
+                extract_first().split())
+        href = response.xpath('//div[@class="profile-nav"]/a/@href').\
+                extract_first()
+        uid = ''.join(re.findall('[0-9]*',href))
+        dataUrl = 'https://m.douban.com/rexxar/api/v2/user/' + uid
+        followedUrl = 'https://m.douban.com/rexxar/api/v2/user/' \
+                + uid + '/following?start=0'
+        yield scrapy.Request(url=dataUrl, meta={'item':item}, \
+                callback=self.getInfo)
+        yield scrapy.Request(url=followedUrl, \
+                callback=self.getFollowingUsers)
+
     def getInfo(self, response):
-        # gets user info, returns item
-        pass
+        item = response.meta['item']
+        data = json.loads(response.text)
+        collection = data['collected_subjects_count']
+        url = data['url']
+        following_count = data['following_count']
+        broadcast = data['statuses_count']
+        doulists = data['owned_doulist_count']
+        item['FollowingNumber'] = following_count
+        item['BroadcastNumber'] = broadcast
+        item['DoulistsNumber'] = doulists
+        item['CollectionNumber'] = collection
+        item['HomeUrl'] = url
+        yield item
 
     def getFollowingUsers(self, response):
-        # gets following users link, return link list
-        pass
+        url = response.url
+        urls = url.split('=')
+        count = int(urls[-1])
+        count += 20
+        data = json.loads(response.text)
+        users = data['users']
+        if users != []:
+            for user in users:
+                userLink = user['url'].replace('www', 'm')
+                yield scrapy.Request(url=userLink,callback=self.parse)
+            url = urls[0] + '=' + str(count)
+            yield scrapy.Request(url=url, callback=self.getFollowingUsers)
