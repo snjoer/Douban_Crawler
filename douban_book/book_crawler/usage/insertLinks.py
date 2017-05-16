f0ae7f0f54bd0f238d42ecad681a90de7045de23
Rafael-Cheng <rafaelcheng13@gmail.com>
2017-05-16 11:10:11 +0800

    fix bug

--- douban_book/book_crawler/usage/insertLinks.py
+++ douban_book/book_crawler/usage/insertLinks.py
@@ -8,4 +8,4 @@ r = redis.Redis(host='localhost', port=6379)
 with open('review_links', 'r') as f:
     lines = f.readlines()
     for line in lines:
-        r.lpush('review_links', line)
+        r.lpush('review_links', line.replace('\n'), '')
